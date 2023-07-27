d3cfc1366b95d10e36919e24358db8fee55b3a34
강민주 <blackdole@naver.com>
2023-07-27 15:40:00 +0900

    .

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main>
       <NavBar />
-      {children}
+      <div className="max-w-2xl mx-auto px-6 pt-4">
+        {children}
+      </div>
       <Footer />
     </main>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { ThemeProvider } from "next-themes";
+import { Toaster } from "react-hot-toast";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 
@@ -11,8 +12,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <ThemeProvider attribute="class">
+      <ThemeProvider attribute="class" enableSystem={true} defaultTheme={'light'}>
         <Component {...pageProps} />
+        <Toaster />
       </ThemeProvider>
     </SessionProvider>
   );
